efe8eb3b5cb6742fdac68975c3274477e7ff0157
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-02-01 23:24:52 +0000

    Continuation of the queries corrections

--- frontend/.innerStudy/src/components/footer.jsx
+++ frontend/.innerStudy/src/components/footer.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import FooterM from "./footer.module.css";
 
 function Footer() {
-  return <div className={FooterM.footer}></div>;
+  return (
+    <div className={FooterM.footer}>
+      <div className={FooterM.text4}>
+        <div className={FooterM.text1}>Checkpoint 4 -</div>
+        <a href="https://www.wildcodeschool.com">
+          <img
+            className={FooterM.text3}
+            src="../../src/assets/images/logo.png"
+            alt="WCS"
+          />
+        </a>
+      </div>
+      <div className={FooterM.text2}>Diogo Oliveira</div>
+    </div>
+  );
 }
 
 export default Footer;
