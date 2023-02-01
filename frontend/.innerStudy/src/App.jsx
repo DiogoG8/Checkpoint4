3c698ffc0bcc38df4458f4ef3aad11c278897827
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-02-01 07:26:47 +0000

    Created a context for a future usage of a token and provide it

--- frontend/.innerStudy/src/App.jsx
+++ frontend/.innerStudy/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import MainPage from "./pages/Main Page/MainPage";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
@@ -5,21 +6,31 @@ import ContactUs from "./pages/Contact Us/ContactUs";
 import MyProfile from "./pages/My Profile/MyProfile";
 import StudyMaterials from "./pages/Study Materials/StudyMaterials";
 import StudyMaterialsSingle from "./pages/Study Materials/StudyMaterialsSingle";
+import TokenContext from "./contexts/authtoken";
 
 import { Link, Route, Router, Routes } from "react-router-dom";
 
 function App() {
+  const [authToken, setAuthToken] = useState();
+
   return (
     <>
-      <Routes>
-        <Route path="*" element={<Login />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/mainpage" element={<MainPage />} />
-        <Route path="/myprofile" element={<MyProfile />} />
-        <Route path="/studymaterials" element={<StudyMaterials />} />
-        <Route path="/studymaterials/:id" element={<StudyMaterialsSingle />} />
-      </Routes>
+      <TokenContext.Provider
+        value={{ authToken: authToken, setAuthToken: setAuthToken }}
+      >
+        <Routes>
+          <Route path="*" element={<Login />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/mainpage" element={<MainPage />} />
+          <Route path="/myprofile" element={<MyProfile />} />
+          <Route path="/studymaterials" element={<StudyMaterials />} />
+          <Route
+            path="/studymaterials/:id"
+            element={<StudyMaterialsSingle />}
+          />
+        </Routes>
+      </TokenContext.Provider>
     </>
   );
 }
