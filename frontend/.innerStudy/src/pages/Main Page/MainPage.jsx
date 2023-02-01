47958a3db132f8aad2232768e878ae1c2df28d21
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-02-01 07:29:39 +0000

    Logout (with token) on the Main Page and Corrections

--- frontend/.innerStudy/src/pages/Main Page/MainPage.jsx
+++ frontend/.innerStudy/src/pages/Main Page/MainPage.jsx
@@ -1,6 +1,15 @@
+import { useState, useContext } from "react";
 import { Link, Navigate } from "react-router-dom";
+import TokenContext from "../../contexts/authtoken";
 
 function MainPage() {
+  const { authToken, setAuthToken } = useContext(TokenContext);
+
+  const logoutHandler = () => {
+    setAuthToken(undefined);
+    localStorage.clear();
+  };
+
   return (
     <>
       <button>
@@ -15,7 +24,7 @@ function MainPage() {
       <button>
         <Link to="/contactus">Contact Us</Link>
       </button>
-      <button>
+      <button onClick={logoutHandler}>
         <Link to="*">Log Out</Link>
       </button>
     </>
