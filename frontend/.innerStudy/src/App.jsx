4febf6dd24913a1182aefb515bdc3a2c43b6f37d
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-02-01 07:18:29 +0000

    Routing builds on App.jsx and Main.jsx

--- frontend/.innerStudy/src/App.jsx
+++ frontend/.innerStudy/src/App.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import MainPage from "./pages/Main Page/MainPage";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import ContactUs from "./pages/Contact Us/ContactUs";
+import MyProfile from "./pages/My Profile/MyProfile";
+import StudyMaterials from "./pages/Study Materials/StudyMaterials";
+import StudyMaterialsSingle from "./pages/Study Materials/StudyMaterialsSingle";
 
-function App() {}
+import { Link, Route, Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<Login />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/studymaterials" element={<StudyMaterials />} />
+        <Route path="/studymaterials/:id" element={<StudyMaterialsSingle />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
