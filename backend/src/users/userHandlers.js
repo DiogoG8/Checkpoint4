a2b7bbed835b0b221a568ec3f7dae6489445c717
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2023-02-03 12:05:02 +0000

    changes

--- backend/src/users/userHandlers.js
+++ backend/src/users/userHandlers.js
@@ -98,6 +98,7 @@ const verifyEmailandPassword = (req, res) => {
               delete req.user.hashedPassword;
               res.send({ token, user: req.user });
             } else {
+              console.error(err);
             }
           })
           .catch((err) => {
